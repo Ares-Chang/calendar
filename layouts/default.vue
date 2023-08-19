5914e9b665d46540182c8ad8fb6e0b9aa0aa08cb
chang <2622310479@qq.com>
2023-08-19 18:32:05 +0800

    chore: init

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <main px-10 py-20 text-center>
+    <slot />
+    <Footer />
+    <div mx-auto mt-5 text-center text-sm opacity-25>
+      [Default Layout]
+    </div>
+  </main>
+</template>
