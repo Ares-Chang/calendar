96b3d1b5bc34402c50513a50c7eec92c6305d73d
chang <2622310479@qq.com>
2023-08-27 19:26:10 +0800

    feat: Created Titlebar components

---
+++ components/Titlebar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div flex items-center justify-between p-4 border="b zinc-7">
+    <h2 text-2xl font-bold>
+      Titlebar
+    </h2>
+
+    <div text="xl gray-4" flex justify-center gap-3>
+      <a i-carbon-logo-github href="https://github.com/Ares-Chang/calendar" target="_blank" />
+      <DarkToggle />
+    </div>
+  </div>
+</template>
--- layouts/home.vue
+++ layouts/home.vue
@@ -1,9 +1,11 @@
 <template>
   <main h-full flex>
     <Navbar h-full w-80 />
-    <div w-full p-4>
-      <slot />
-      <Footer />
+    <div w-full flex="~ col">
+      <Titlebar />
+      <div flex-1 overflow-auto p-4>
+        <slot />
+      </div>
     </div>
   </main>
 </template>
