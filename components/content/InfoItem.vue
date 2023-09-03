08a8d2b5693f90f9f9b12c2a9af1e73fd6cdd88f
chang <2622310479@qq.com>
2023-09-03 16:02:19 +0800

    feat: Created InfoItem Component

---
+++ components/content/InfoItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const selected = ref(true)
+const value = ref('')
+</script>
+
+<template>
+  <UCard :ui="{ body: { padding: 'p-4' } }">
+    <div flex items-center gap-2>
+      <UCheckbox v-model="selected" name="notifications" />
+      <UInput v-model="value" variant="none" :ui="{ wrapper: 'w-full' }" />
+    </div>
+  </UCard>
+</template>
--- components/content/index.vue
+++ components/content/index.vue
@@ -3,10 +3,9 @@
 </script>
 
 <template>
-  <div>
-    content
-    <UBadge color="white" variant="solid">
-      Badge
-    </UBadge>
+  <div flex="~ col" h-full gap-3 overflow-auto p-0.5>
+    <div v-for="item in 10" :key="item">
+      <ContentInfoItem />
+    </div>
   </div>
 </template>
