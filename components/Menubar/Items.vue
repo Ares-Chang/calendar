f8c5d092aef3d6811d883817e46e2d86bd165d33
chang <2622310479@qq.com>
2023-09-09 16:26:12 +0800

    feat: create focus Menu items

--- components/Menubar/Items.vue
+++ components/Menubar/Items.vue
@@ -3,6 +3,19 @@ const { list, line } = defineProps<{
   list: MenusList
   line: boolean
 }>()
+
+type Items = Element | ComponentPublicInstance | null
+
+const items = ref<Record<string, Items>>({})
+
+watch(items, () => {
+  list.children.forEach((item, index) => {
+    if (item.focus)
+      (items.value[index] as HTMLElement).focus()
+  })
+}, {
+  deep: true,
+})
 </script>
 
 <template>
@@ -17,8 +30,11 @@ const { list, line } = defineProps<{
 
     <div
       v-for="({ icon, label }, index) in list.children" :key="index"
+      :ref="(el: Items) => items[index] = el"
+      tabindex="0" hidefocu
       bg="hover:gray-200 dark:hover:gray-800"
       ml-2 flex cursor-pointer items-center gap-2 rd-1 p-2
+      focus:outline="1 green solid"
     >
       <i inline-block :class="icon" />
       <span>{{ label }}</span>
