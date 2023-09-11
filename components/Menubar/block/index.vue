b5a775f51fc4f0927886458f4d3e38d0046e52bc
chang <2622310479@qq.com>
2023-09-11 17:18:38 +0800

    perf: created Menubar block items Components

--- components/Menubar/block/index.vue
+++ components/Menubar/block/index.vue
@@ -3,19 +3,6 @@ const { list, line } = defineProps<{
   list: MenusList
   line: boolean
 }>()
-
-type Items = Element | ComponentPublicInstance | null
-
-const items = ref<Record<string, Items>>({})
-
-watch(items, () => {
-  list.children.forEach((item, index) => {
-    if (item.focus)
-      (items.value[index] as HTMLElement).focus()
-  })
-}, {
-  deep: true,
-})
 </script>
 
 <template>
@@ -29,15 +16,9 @@ watch(items, () => {
     </div>
 
     <div
-      v-for="({ icon, label }, index) in list.children" :key="index"
-      :ref="(el: Items) => items[index] = el"
-      tabindex="0" hidefocu
-      bg="hover:gray-200 dark:hover:gray-800"
-      ml-2 flex cursor-pointer items-center gap-2 rd-1 p-2
-      focus:outline="1 green solid"
+      v-for="(item, index) in list.children" :key="index"
     >
-      <i inline-block :class="icon" />
-      <span>{{ label }}</span>
+      <MenubarBlockItems :data="item" />
     </div>
 
     <div v-if="line" mt-1 border="b gray-400/50 dark:gray-700/50" />
---
+++ components/Menubar/block/items.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const { data } = defineProps<{
+  data: MenusItem
+}>()
+
+const block = ref(null)
+const { focused } = useFocus(block)
+
+onMounted(() => {
+  focused.value = data.focus
+})
+</script>
+
+<template>
+  <div
+    ref="block"
+    tabindex="0" hidefocu
+    bg="hover:gray-200 dark:hover:gray-800"
+    ml-2 flex cursor-pointer items-center gap-2 rd-1 p-2
+    focus:outline="1 green solid"
+  >
+    <i inline-block :class="data.icon" />
+    <span>{{ data.label }}</span>
+  </div>
+</template>
