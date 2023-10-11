94550d4ea663e0a845820be11887c47a7ab03e38
chang <2622310479@qq.com>
2023-10-11 17:13:41 +0800

    feat: add loading page

---
+++ components/Loading.vue
@@ -0,0 +1,8 @@
+<template>
+  <div h-full w-full flex="~ col" items-center justify-center gap-4>
+    <img src="/loading.gif" alt="Loading Content...">
+    <p text-2xl font-bold font-mono>
+      Read data loading...
+    </p>
+  </div>
+</template>
