735397a8bcab6958547d2d51adcb5023394c1f36
chang <areschang114@qq.com>
2023-11-25 13:23:10 +0800

    chore: update deps & migrate to new eslint format

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <main px-10 py-20 text-center>
     <slot />
     <Footer />
-    <div mx-auto mt-5 display-none text-center text-sm opacity-25>
+    <div display-none mx-auto mt-5 text-center text-sm opacity-25>
       [Default Layout]
     </div>
   </main>
